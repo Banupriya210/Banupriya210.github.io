d30820deb9fddcfae858dd85a891615c20377b84
Banupriya210 <banutharacs@gmail.com>
2020-10-21 22:01:40 +0530

    add new file

---
+++ app.js
@@ -0,0 +1,32 @@
+/*var x=10,y=20,z;
+z=x;
+x=y;
+y=z;
+console.log(x,y);
+console.log("x="+x,"y="+y);*/
+
+/*var a=[2,7,3,5];
+console.log("push method");
+a.push(10,17);
+for(i=0;i<a.length;i++)
+{
+    console.log(a[i]);
+}
+console.log("pop method");
+a.pop();
+for(i=0;i<a.length;i++)
+{
+    console.log(a[i]);
+}
+console.log("unshift");
+a.unshift(12,10);
+for(i=0;i<a.length;i++)
+{
+    console.log(a[i]);
+}
+console.log("shift");
+a.shift()
+for(i=0;i<a.length;i++)
+{
+    console.log(a[i]);
+}*/
