6cffb8ad71e773b4bdcd2290d7069070505fad5b
Banupriya210 <banutharacs@gmail.com>
2020-10-22 09:41:41 +0530

    added in new file

--- app.js
+++ app.js
@@ -30,3 +30,54 @@ for(i=0;i<a.length;i++)
 {
     console.log(a[i]);
 }*/
+/*console.log("array concat");
+var b=[3,6,8,3,7],i;
+var c=[3,7,4,9,2];
+var d=b.concat(c);
+for(i=0;i<d.length;i++)
+{
+    console.log(d[i]);
+}
+console.log("indexOf");
+var a=[5,8,6,9,3,6],i;
+for(i=0;i<a.length;i++)
+{
+   
+    console.log(a.indexOf(6,a.indexOf(6)+1));
+}
+console.log("last indexof");
+var a=[3,4,5,7,8],i;
+for(i=0;i<a.length;i++)
+{
+    console.log(a.lastIndexOf(8));
+}
+console.log("reverse");
+var b=[2,4,3,5,6,7],i;
+i=b.reverse(a[i]);
+for(i=0;i<b.length;i++)
+{
+    console.log(b[i]);
+}
+console.log("sort");
+var c=[2,5,4,7,6,2],i;
+i=c.sort(c[i]);
+for(i=0;i<c.length;i++)
+{4
+    console.log(c[i]);
+}*/
+console.log("slice");
+var d=[1,5,6,7,4],i;
+for(i=0;i<d.length;i++)
+{
+    console.log(d.slice(2));
+}
+/*console.log("splice");
+var c=[1,5,4,8,7];
+    c.splice(2,0,17);
+    console.log(c);*/
+/*console.log("tostringof");
+var a=[3,4,5,6,8],i;
+i=a.tostringof(a[i]);
+console.log(i);*/
+
+
